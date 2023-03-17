6aab03900edc0d55d1bf33152c3696bacd12e899
Kristyn Le <kristynle0608@gmail.com>
2023-03-17 18:41:30 -0300

    fix log in page

--- library/src/main/LogInPage.java
+++ library/src/main/LogInPage.java
@@ -3,9 +3,15 @@ package main;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -19,14 +25,15 @@ public class LogInPage extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        BorderPane bPane = new BorderPane();
-        VBox vBox = addLogin();
 
-        // for login
-        bPane.setCenter(addLogin());
+        BorderPane borderPane = new BorderPane();
+        HBox hBox = addHBox();
+        borderPane.setTop(hBox);
+
+        borderPane.setCenter(addGridPane());
 
         // Create scene
-        Scene scene = new Scene(bPane);
+        Scene scene = new Scene(borderPane, 700, 400);
 
         primaryStage.setTitle("Library - Login");
         primaryStage.setScene(scene);
@@ -34,18 +41,47 @@ public class LogInPage extends Application {
 
     }
 
-    private VBox addLogin() {
-        VBox vBox = new VBox();
-        vBox.setSpacing(20.0);
+    private GridPane addGridPane() {
+        GridPane gridPane = new GridPane();
 
-        Label loginLabel = new Label("Login");
+        gridPane.setPadding(new Insets(30));
+        gridPane.setHgap(25);
+        gridPane.setVgap(20);
+
+        Label loginLabel = new Label("Username");
         TextField loginText = new TextField();
 
         Label passwordLabel = new Label("Password");
         PasswordField passwordField = new PasswordField();
 
-        vBox.getChildren().addAll(loginLabel, loginText, passwordLabel, passwordField);
+        Button submitButton = new Button("Submit");
 
-        return vBox;
+        gridPane.add(loginLabel, 0, 0);
+        gridPane.add(loginText, 1, 0);
+        gridPane.add(passwordLabel, 0,1);
+        gridPane.add(passwordField, 1, 1);
+        gridPane.add(submitButton, 0, 2);
+
+        gridPane.setAlignment(Pos.CENTER);
+
+        return gridPane;
     }
+
+    private HBox addHBox() {
+        HBox hBox = new HBox();
+        hBox.setPadding(new Insets(50, 20, 0 , 20));
+        hBox.setSpacing(10);
+
+        Label title = new Label("Library - Login Page");
+        title.setFont(Font.font(40));
+        hBox.getChildren().add(title);
+        hBox.setAlignment(Pos.CENTER);
+
+        return hBox;
+    }
+
+    private boolean validateLogin() {
+        
+    }
+
 }
