4d7820c5ee09fbd265744dd90c38f93111a8d831
Kristyn Le <kristynle0608@gmail.com>
2023-03-22 18:50:44 -0300

    Create AddBooksPage.java

---
+++ library/src/main/AddBooksPage.java
@@ -0,0 +1,23 @@
+package main;
+
+import javafx.application.Application;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class AddBooksPage extends Application {
+
+    private Library library;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+    private static GridPane addGridPane() {
+        
+    }
+}
